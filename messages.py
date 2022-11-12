1f5f2f2be1567712a89776b39f88ac932fca38f8
MaciejKaca <maciek.kaca@gmail.com>
2022-11-12 09:56:13 -0800

    Code refactor

--- messages.py
+++ messages.py
@@ -2,6 +2,7 @@ from enum import Enum
 import time
 import json
 
+
 class MessageType(Enum):
     NULL = 1
     SPEED = 2
@@ -10,15 +11,18 @@ class MessageType(Enum):
     JOYSTICK = 5
     HEARTBEAT = 6
 
+
 class MessageFields(Enum):
     MESSAGE_TYPE = 1
     SENT_TIME = 2
     DATA = 3
 
+
 class Direction(Enum):
     FORWARD = 1
     BACKWARD = 2
     BRAKE = 3
+
 
 def deserialize_message(message):
     message = message.decode("utf-8")
@@ -32,18 +36,20 @@ def deserialize_message(message):
         data = JoystickData()
     elif message_type == MessageType.HEARTBEAT:
         data = Heartbeat()
-    
+
     data.deserialize(message_dict)
     return data
 
+
 def get_time():
     return round(time.time() * 1000)
+
 
 class DataMessage:
     def __init__(self):
         self.message_type = MessageType.NULL
         self.sent_time = get_time()
-        pass 
+        pass
 
     def serialize(self) -> str:
         pass
@@ -54,11 +60,13 @@ class DataMessage:
     def __get_data(self) -> dict:
         pass
 
-    def __set_data(self, data : dict):
+    def __set_data(self, data: dict):
         pass
+
 
 class SpeedData(DataMessage):
     def __init__(self):
+        super().__init__()
         self.speed = 0
         self.direction = Direction.FORWARD
         self.message_type = MessageType.SPEED
@@ -70,13 +78,14 @@ class SpeedData(DataMessage):
         data['direction'] = self.direction.value
         return data
 
-    def __set_data(self, data : dict):
+    def __set_data(self, data: dict):
         self.speed = int(data.get('speed'))
         self.direction = Direction(int(data.get('direction')))
 
     def serialize(self) -> str:
         data = self.__get_data()
-        message = {MessageFields.MESSAGE_TYPE.name : self.message_type.value, MessageFields.SENT_TIME.name: self.sent_time, MessageFields.DATA.name: data}
+        message = {MessageFields.MESSAGE_TYPE.name: self.message_type.value,
+                   MessageFields.SENT_TIME.name: self.sent_time, MessageFields.DATA.name: data}
 
         return json.dumps(message)
 
@@ -87,47 +96,52 @@ class SpeedData(DataMessage):
         data = dict(message.get(MessageFields.DATA.name))
         self.__set_data(data)
 
+
 class JoystickData(DataMessage):
-        def __init__(self):
-            self.event_type = 0
-            self.value = 0
-            self.axis = 0
-            self.button = 0
-            self.message_type = MessageType.JOYSTICK
-            self.sent_time = get_time()
+    def __init__(self):
+        super().__init__()
+        self.event_type = 0
+        self.value = 0
+        self.axis = 0
+        self.button = 0
+        self.message_type = MessageType.JOYSTICK
+        self.sent_time = get_time()
 
-        def __set_data(self, data : dict):
-            self.event_type = int(data.get('event_type'))
-            self.axis = int(data.get('axis'))
-            if str(data.get('value')).find(".") == -1:
-                self.value = int(data.get('value'))
-            else:
-                self.value = float(data.get('value'))
-            self.button = int(data.get('button'))
+    def __set_data(self, data: dict):
+        self.event_type = int(data.get('event_type'))
+        self.axis = int(data.get('axis'))
+        if str(data.get('value')).find(".") == -1:
+            self.value = int(data.get('value'))
+        else:
+            self.value = float(data.get('value'))
+        self.button = int(data.get('button'))
 
-        def __get_data(self):
-            data = {}
-            data['event_type'] = str(self.event_type)
-            data['value'] = str(self.value)
-            data['axis'] = str(self.axis)
-            data['button'] = str(self.button)
-            return data
+    def __get_data(self):
+        data = {}
+        data['event_type'] = str(self.event_type)
+        data['value'] = str(self.value)
+        data['axis'] = str(self.axis)
+        data['button'] = str(self.button)
+        return data
 
-        def serialize(self) -> str:
-            data = self.__get_data()
-            message = {MessageFields.MESSAGE_TYPE.name : self.message_type.value, MessageFields.SENT_TIME.name: self.sent_time, MessageFields.DATA.name: data}
+    def serialize(self) -> str:
+        data = self.__get_data()
+        message = {MessageFields.MESSAGE_TYPE.name: self.message_type.value,
+                   MessageFields.SENT_TIME.name: self.sent_time, MessageFields.DATA.name: data}
 
-            return json.dumps(message)
+        return json.dumps(message)
 
-        def deserialize(self, message):
-            self.message_type = MessageType(message.get(MessageFields.MESSAGE_TYPE.name))
-            self.sent_time = int(message.get(MessageFields.SENT_TIME.name))
+    def deserialize(self, message):
+        self.message_type = MessageType(message.get(MessageFields.MESSAGE_TYPE.name))
+        self.sent_time = int(message.get(MessageFields.SENT_TIME.name))
 
-            data = dict(message.get(MessageFields.DATA.name))
-            self.__set_data(data)
+        data = dict(message.get(MessageFields.DATA.name))
+        self.__set_data(data)
+
 
 class Heartbeat(DataMessage):
     def __init__(self):
+        super().__init__()
         self.system_id = 0
         self.message_type = MessageType.HEARTBEAT
         self.sent_time = get_time()
@@ -137,12 +151,13 @@ class Heartbeat(DataMessage):
         data['system_id'] = self.system_id
         return data
 
-    def __set_data(self, data : dict):
+    def __set_data(self, data: dict):
         self.system_id = int(data.get('system_id'))
 
     def serialize(self) -> str:
         data = self.__get_data()
-        message = {MessageFields.MESSAGE_TYPE.name : self.message_type.value, MessageFields.SENT_TIME.name: self.sent_time, MessageFields.DATA.name: data}
+        message = {MessageFields.MESSAGE_TYPE.name: self.message_type.value,
+                   MessageFields.SENT_TIME.name: self.sent_time, MessageFields.DATA.name: data}
 
         return json.dumps(message)
 
